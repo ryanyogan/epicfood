a6e8e68b19bb8d446b7049a0ee8326c96f0faa70
Ryan Yogan <ryanyogan@gmail.com>
2015-10-16 20:05:49 -0500

    basic entry point for iOS app

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+import React from 'react-native';
+import { Provider } from 'react-redux/native';
+import App from './src/containers/App';
+import configureStore from './src/store/configureStore';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+const {
+  Component,
+  AppRegistry
 } = React;
 
-var epicfood = React.createClass({
-  render: function() {
+// Redux - Single state stree store
+const store = configureStore();
+
+class Albums extends Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        {() => <App />}
+      </Provider>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('epicfood', () => epicfood);
+AppRegistry.registerComponent('Albums', () => Albums);
